319d3b56ec299678b559a4aa6611c063f7de0365
Alex Carstensen <matthacker.alex@gmail.com>
2015-10-20 14:06:31 +0200

    added code form the C# Station ADO.NET lesson, SQLCommands.cs and SQLConnectionDemo.cs
    changed connection string and updated readdata and insertdata

---
+++ HandIn2/Console Application/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database_Access_Logic_Tier;
+
+namespace Console_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                SQLCommands scd = new SQLCommands();
+
+                Console.WriteLine();
+                Console.WriteLine("Categories Before Insert");
+                Console.WriteLine("------------------------");
+
+                // use ExecuteReader method
+                scd.ReadData();
+            /*
+                // use ExecuteNonQuery method for Insert
+                scd.Insertdata();
+                Console.WriteLine();
+                Console.WriteLine("Categories After Insert");
+                Console.WriteLine("------------------------------");
+
+                scd.ReadData();
+            
+                // use ExecuteNonQuery method for Update
+                scd.UpdateData();
+
+                Console.WriteLine();
+                Console.WriteLine("Categories After Update");
+                Console.WriteLine("------------------------------");
+
+                scd.ReadData();
+
+                // use ExecuteNonQuery method for Delete
+                scd.DeleteData();
+
+                Console.WriteLine();
+                Console.WriteLine("Categories After Delete");
+                Console.WriteLine("------------------------------");
+
+                scd.ReadData();
+
+                // use ExecuteScalar method
+                int numberOfRecords = scd.GetNumberOfRecords();
+
+                Console.WriteLine();
+                Console.WriteLine("Number of Records: {0}", numberOfRecords);*/
+            
+        }
+    }
+}
---
+++ HandIn2/Database Access Logic Tier/SQLCommands.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Database_Access_Logic_Tier
+{
+    public class SQLCommands
+    {
+        SqlConnection _conn;
+
+        public SQLCommands()
+        {
+            // Instantiate the connection
+            _conn = new SqlConnection(
+               @"Data Source=(localdb)\ProjectsV12;Initial Catalog=Handin2SSDT;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        // call methods that demo SqlCommand capabilities
+       
+
+        /// <summary>
+        /// use ExecuteReader method
+        /// </summary>
+        public void ReadData()
+        {
+            SqlDataReader rdr = null;
+
+            try
+            {
+                // Open the connection
+                _conn.Open();
+
+                // 1. Instantiate a new command with a query and connection
+                SqlCommand cmd = new SqlCommand(@"SELECT * FROM bor INNER JOIN Adresse ON(bor.AdresseID = Adresse.AdresseID)
+                  INNER JOIN Person ON(bor.PersonID = Person.PersonID)
+                  INNER JOIN Telefon ON(Person.PersonID = Telefon.PersonID)", _conn);
+
+                
+                // 2. Call Execute reader to get query results
+                rdr = cmd.ExecuteReader();
+                Console.WriteLine(rdr.FieldCount);
+                // print the CategoryName of each record
+                while (rdr.Read())
+                {
+                    for (int i = 0; i < rdr.FieldCount; i++)
+                    {
+                        Console.Write(rdr[i] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            finally
+            {
+                // close the reader
+                rdr?.Close();
+
+                // Close the connection
+                _conn?.Close();
+            }
+        }
+
+        /// <summary>
+        /// use ExecuteNonQuery method for Insert
+        /// </summary>
+        public void Insertdata()
+        {
+            try
+            {
+                // Open the connection
+                _conn.Open();
+
+                SqlDataReader pID = null;
+                SqlDataReader aID = null;
+
+                // prepare command string
+                string[] insertString =
+                {
+                    @"INSERT INTO Adresse ([Type], VejNavn, HusNummer, PostNummer, City) VALUES ( 'Skole', 'Svendborgvej', 10, 5432, 'Svendborg')",
+                    @"SELECT AdressID FROM Adresse WHERE VejNavn = 'Svendborgvej' AND HusNummer = 10",
+                    @"INSERT INTO Person ([CPRNr], [Fornavn], [Mellemnavn], [Efternavn], [type], [AdresseID]) VALUES ( 1245365243, 'Jonathan', 'Parkour', 'Spang', 'Skole',@AdressID)",
+                    @"SELECT PersonID FROM Person WHERE CPRNr = 1245365243 AND Fornavn = 'Jonathan'"};
+                
+               
+                
+               
+
+                // 1. Instantiate a new command with a query and connection
+                SqlCommand cmd = new SqlCommand(insertString[0], _conn);
+
+                // 2. Call ExecuteNonQuery to send command
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqlCommand(insertString[1], _conn);
+                pID = cmd.ExecuteReader();
+
+                cmd = new SqlCommand(insertString[2], _conn);
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqlCommand(insertString[3], _conn);
+                aID = cmd.ExecuteReader();
+
+                cmd = new SqlCommand("INSERT INTO Telefon (PersonID, TelefonNr, [Type]) VALUES ("+ pID +", 21535625, 'Skole')", _conn);
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqlCommand("INSERT INTO bor (PersonID, AdresseID) VALUES (" + pID + ", " + aID + ")", _conn);
+                cmd.ExecuteNonQuery();
+
+            }
+            finally
+            {
+                // Close the connection
+                if (_conn != null)
+                {
+                    _conn.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// use ExecuteNonQuery method for Update
+        /// </summary>
+        public void UpdateData()
+        {
+            /*try
+            {
+                // Open the connection
+                _conn.Open();
+
+                // prepare command string
+                string updateString = @"
+                 update Categories
+                 set CategoryName = 'Other'
+                 where CategoryName = 'Miscellaneous'";
+
+                // 1. Instantiate a new command with command text only
+                SqlCommand cmd = new SqlCommand(updateString);
+
+                // 2. Set the Connection property
+                cmd.Connection = conn;
+
+                // 3. Call ExecuteNonQuery to send command
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }*/
+        }
+
+        /// <summary>
+        /// use ExecuteNonQuery method for Delete
+        /// </summary>
+        public void DeleteData()
+        {
+           /* try
+            {
+                // Open the connection
+                conn.Open();
+
+                // prepare command string
+                string deleteString = @"
+                 delete from Categories
+                 where CategoryName = 'Other'";
+
+                // 1. Instantiate a new command
+                SqlCommand cmd = new SqlCommand();
+
+                // 2. Set the CommandText property
+                cmd.CommandText = deleteString;
+
+                // 3. Set the Connection property
+                cmd.Connection = conn;
+
+                // 4. Call ExecuteNonQuery to send command
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }*/
+        }
+
+        /// <summary>
+        /// use ExecuteScalar method
+        /// </summary>
+        /// <returns>number of records</returns>
+       /* public int GetNumberOfRecords()
+        {
+            int count = -1;
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command
+                SqlCommand cmd = new SqlCommand("select count(*) from Categories", conn);
+
+                // 2. Call ExecuteScalar to send command
+                count = (int)cmd.ExecuteScalar();
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return count;
+        }*/
+    }
+}
